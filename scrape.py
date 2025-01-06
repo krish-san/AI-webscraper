70f6446ddcc863740d9e545d04a72932047558d4
san <120664435+krish-san@users.noreply.github.com>
2025-01-06 21:26:04 +0530

    specified website with proxy for captcha handling

---
+++ scrape.py
@@ -0,0 +1,22 @@
+from selenium.webdriver import Remote, ChromeOptions
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+
+SBR_WEBDRIVER = 'https://brd-customer-hl_347b7f41-zone-ai_scraper-country-us:x8zf0zaorkut@brd.superproxy.io:9515'
+
+
+def scrape_web(website):
+    print("launching browser...")
+    print('Connecting to Scraping Browser...')
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    with Remote(sbr_connection, options=ChromeOptions()) as driver:
+        driver.get('https://www.amazon.com')
+        # CAPTCHA handling
+        print('Waiting captcha to solve...')
+        solve_res = driver.execute('executeCdpCommand', {
+            'cmd': 'Captcha.waitForSolve',
+            'params': {'detectTimeout': 10000},
+        })
+        print('Captcha solve status:', solve_res['value']['status'])
+        print('Navigated! Scraping page content...')
+        html = driver.page_source
+        return html
