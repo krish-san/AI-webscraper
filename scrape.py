41c4a4cd158dde0ea5c1a7edfcbcbd144c8587ca
san <120664435+krish-san@users.noreply.github.com>
2025-01-07 23:41:26 +0530

    DOM content extraction function is added

--- scrape.py
+++ scrape.py
@@ -9,7 +9,7 @@ def scrape_web(website):
     print('Connecting to Scraping Browser...')
     sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
     with Remote(sbr_connection, options=ChromeOptions()) as driver:
-        driver.get('https://www.amazon.com')
+        driver.get(website)
         # CAPTCHA handling
         print('Waiting captcha to solve...')
         solve_res = driver.execute('executeCdpCommand', {
@@ -20,3 +20,24 @@ def scrape_web(website):
         print('Navigated! Scraping page content...')
         html = driver.page_source
         return html
+
+def extract_body(html_content):
+    soup=BeautifulSoup(html_content,"html.parser")
+    body_content=soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+def clean_body(body_content):
+    soup=BeautifulSoup(body_content,"html.parser")
+    for script_nd_style in soup(["script","style"]):
+        script_nd_style.extract()
+
+    cleaned_content=soup.get_text(separator="\n")
+    cleaned_content="\n".join(line.strip() for line in cleaned_content.splitlines() if line.strip())
+
+    return cleaned_content 
+
+def split_dom_content(dom_content, maxlen=6000):
+    return [
+        dom_content[i: i + maxlen] for i in range(0,len(dom_content),maxlen)
+    ]
