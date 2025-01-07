0160e4b972a126872e04fde86cc27d0ce8cb18d1
san <120664435+krish-san@users.noreply.github.com>
2025-01-07 23:43:10 +0530

    imported scrape functions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from scrape import scrape_web
+from scrape import scrape_web,extract_body,clean_body,split_dom_content
 
 st.title("AI webscraper")
 
@@ -8,4 +8,17 @@ url=st.text_input("Enter a Website URL: ")
 if st.button("scrape"):
     st.write("scraping")
     res=scrape_web(url)
-    print(res)
+    body_content=extract_body(res)
+    cleaned_content=clean_body(res)
+
+    st.session_state.dom_content=cleaned_content
+    with st.expander("View DOM Content"):  #button
+        st.text_area("DOM Content ",cleaned_content,height=350)
+
+    if "dom_content" in st.session_state:
+         parse_des=st.text_area("Specify the content you to parse .")
+         if st.button("Parse Content"):
+             if parse_des:
+                 st.write("parsing the content")
+
+                 dom_chunks=split_dom_content(st.session_state.dom_content)
