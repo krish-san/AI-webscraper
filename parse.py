9c250b13dc8135bf276bbe3814f654278e99ed65
san <120664435+krish-san@users.noreply.github.com>
2025-01-07 23:39:47 +0530

    Add files via upload

---
+++ parse.py
@@ -0,0 +1,7 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatMessagePromptTemplate
+
+model=OllamaLLM(model="llama3")
+
+def parse_content(dom_chunks,parse_des):
+    pass
